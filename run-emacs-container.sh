d10741676ee7e31492bc87588016aa8507c11d22
Kostas Zorbadelos <kzorba@nixly.net>
2025-05-09 11:09:19 +0000

    Fix run-emacs-container.sh script.

--- run-emacs-container.sh
+++ run-emacs-container.sh
@@ -2,9 +2,9 @@
 
 export WORKDIR="/w"
 # Start docker daemon before this.
-docker run -d --rm --name ghcr.io/kzorba/terminal-emacs \
+docker run -d --rm --name terminal-emacs \
   -e EMACS_UID=$(id -u) \
   -e EMACS_GID=$(id -g) \
-  -e WORKDIR=$WORKDIR
+  -e WORKDIR=$WORKDIR \
   -v "$HOME":"$WORKDIR" \
-  kzorba/terminal-emacs
+  ghcr.io/kzorba/terminal-emacs
