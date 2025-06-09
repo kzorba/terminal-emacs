685bdc3c02ffb8a152e804559daa54b008e12f98
Kostas Zorbadelos <kzorba@nixly.net>
2025-06-09 16:35:46 +0000

    Set container's hostname to devcon (run-emacs-container.sh)

--- run-emacs-container.sh
+++ run-emacs-container.sh
@@ -3,6 +3,7 @@
 export WORKDIR="/v"
 # Start docker daemon before this.
 docker run -d --rm --name devcon \
+  --hostname devcon \
   -e EMACS_UID=$(id -u) \
   -e EMACS_GID=$(id -g) \
   -e WORKDIR=$WORKDIR \
