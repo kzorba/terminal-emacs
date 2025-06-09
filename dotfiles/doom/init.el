8052629b8d58923653bbab7ea5fa7be34c0b962d
Kostas Zorbadelos <kzorba@nixly.net>
2025-06-09 19:30:00 +0000

    Disable tree-sitter (for now) in doom emacs.
    - Might be that tree-sitter is responsible for display instability of
      buffers in the terminal.
    - Will investigate tree-sitter in a later time.

--- dotfiles/doom/init.el
+++ dotfiles/doom/init.el
@@ -105,7 +105,7 @@
        ;;pdf               ; pdf enhancements
        ;;terraform         ; infrastructure as code
        ;;tmux              ; an API for interacting with tmux
-       tree-sitter       ; syntax and parsing, sitting in a tree...
+       ;;tree-sitter       ; syntax and parsing, sitting in a tree...
        ;;upload            ; map local to remote projects via ssh/ftp
 
        :os
@@ -174,7 +174,7 @@
        ;;swift             ; who asked for emoji variables?
        ;;terra             ; Earth and Moon in alignment for performance.
        ;;web               ; the tubes
-       (yaml +treesitter)              ; JSON, but readable
+       yaml                ; JSON, but readable
        ;;zig               ; C, but simpler
 
        :email
